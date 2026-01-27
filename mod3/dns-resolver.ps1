587fec72d69d46d32c4def228d3393f4fa0ab325
Rune <rune.seregina@mymail.champlain.edu>
2026-01-27 13:58:11 -0500

    dns resolver windows script add

---
+++ mod3/dns-resolver.ps1
@@ -0,0 +1,12 @@
+﻿param ($IP, $server)
+
+for ($i = 1; $i -le 254; $i++) {
+    $HostAddress = "$IP.$i"
+    $HostUp = Resolve-DnsName -Name $HostAddress -Server $server -ErrorAction SilentlyContinue
+
+    foreach ($h in $HostUp) {
+        if ($h.NameHost) {
+        Write-Host "$HostAddress $($h.NameHost)"
+        }
+    }
+}
